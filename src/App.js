8173d3d18b2e70a5c80d01357b73247e7c12f4ec
tuomasesp <101732066+tuomasesp@users.noreply.github.com>
2022-09-15 18:23:00 +0300

    Valmis (tarkastusta vaille)

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 
 function App() {
   const [weight, setWeight] =useState(0);
-  const [bottles, setBottles] = useState(0.33);
-  const [time, setTime] = useState();
+  const [bottles, setBottles] = useState(0);
+  const [time, setTime] = useState(1);
   const [gender, setGender] = useState('male');
   const [result, setResult] = useState(0);
 
@@ -12,14 +12,19 @@ function App() {
     e.preventDefault();
 
     let alcohol = 0;
-    const litres = bottles * 0.33;
-    const grams = litres * 8 * 4.5;
+    let grams = bottles * 8 * 4.5;
+    let left = grams - ((weight/10)*time)
 
     if (gender === 'male') {
-      alcohol = grams / (weight * 0.7);
+      alcohol = left / (weight * 0.7);
     } else {
-      alcohol = grams / (weight * 0.6);
+      alcohol = left / (weight * 0.6);
     }
+
+    if (alcohol< 0) {
+      alcohol = 0;
+    }
+
     setResult(alcohol);
   }
 
@@ -28,7 +33,7 @@ function App() {
   return (
     <>
     <h3>Calculating alcohol blood level</h3>
-    <div>
+    <div className="content">
       <form onSubmit={handleSubmit}>
         <div>
           <label>Weight</label>
@@ -38,18 +43,18 @@ function App() {
           <label>Bottles</label>
           <select name="bottles" value={bottles} onChange={e => setBottles(e.target.value)}>
               <option value="0">0</option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-              <option value="11">11</option>
-              <option value="12">12</option>
+              <option value="0.33">1</option>
+              <option value="0.66">2</option>
+              <option value="0.99">3</option>
+              <option value="1.32">4</option>
+              <option value="1.65">5</option>
+              <option value="1.98">6</option>
+              <option value="2.31">7</option>
+              <option value="2.64">8</option>
+              <option value="2.97">9</option>
+              <option value="3.3">10</option>
+              <option value="3.63">11</option>
+              <option value="3.96">12</option>
             </select>
         </div>
         <div>
@@ -75,7 +80,7 @@ function App() {
           <input type="radio" name="gender" value="female" onChange={e => setGender(e.target.value)} /><label>Female</label>
         </div>
         <div>
-          <output>{result}</output>
+          <output>{result.toFixed(2)}</output>
         </div>
         <button>Calculate</button>
       </form>
